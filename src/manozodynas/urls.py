cfec76ffbc527033bcaa39a5026ca1360cde11df
OniQ <oniq.eg@gmail.com>
2014-05-09 18:33:23 +0300

    Base template. Navigation bar. Word list view. Add new words.

--- src/manozodynas/urls.py
+++ src/manozodynas/urls.py
@@ -4,9 +4,13 @@ from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from .views import index_view
+from .views import WordList
+from .views import WordCreate
 
 urlpatterns = patterns('',
     url(r'^$', index_view, name='index'),
+    url(r'^words$', WordList.as_view(), name='words'),
+    url(r'^word$', WordCreate.as_view(), name='word'),
 )
 
 urlpatterns += staticfiles_urlpatterns()
--- src/manozodynas/views.py
+++ src/manozodynas/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from manozodynas.models import *
+from django.views.generic import CreateView
 
+class WordList(ListView):
+    model = Word
+    paginate_by = 3
+    success_url = 'index/'
+    def get_context_data(self, **kwargs):
+        context = super(WordList, self).get_context_data(**kwargs)
+        return context
+
+class WordCreate(CreateView):
+    model = Word
+    template_name = 'manozodynas/word.html'
+    success_url = 'zodziai/'
 
 def index_view(request):
     return render(request, 'manozodynas/index.html', {})
+
+def words_view(request):
+    words = Word.objects.all()
+    return render(request, 'manozodynas/words.html', {'words': words})
